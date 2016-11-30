a52f6c061392eb61bd42c841a21a1730d77ec848
PC 19 <PC 19>
2016-11-30 17:38:05 -0600

    modifique cordenadas para corregir ensimado de objetos

--- Programas-de-aprendices/rogelio/area-de-un-cuadrado_1.py
+++ Programas-de-aprendices/rogelio/area-de-un-cuadrado_1.py
@@ -6,25 +6,25 @@ pilas.fondos.Tarde()
 
 titulo=pilas.actores.Texto(u"Área de un cubo", y=200, magnitud=30)
 descri=u"""Para calcular el área de un cubo lo que se necesita es conocer la longitud de uno de sus lados y luego aplicar la fórmula:
-   lxlxlxl
+  lxlxlxl
 """
-parrafo1=pilas.actores.Texto(descri, y=160, ancho=400)
-parrafo1.centro_y = 'arriba'
+parrafo1=pilas.actores.Texto(descri, y=170, ancho=400)
+parrafo1.centro_y ='arriba'
 
-etiqueta1=pilas.actores.Texto("lado=",y=60,x=-200)
-etiqueta1.centro_x='izquierda'
-entrada=pilas.interfaz.IngresoDeTexto(texto='0',y=60,x=-130,ancho=50)
+etiqueta1=pilas.actores.Texto("lado=",y=-5,x=-200)
+etiqueta1.centro_X='iquierda'
+entrada=pilas.interfaz.IngresoDeTexto(texto='0',y=-5,x=-110,ancho=50)
 entrada.centro_x='izquierda'
 
 area=0
 def calcular_area():
     lado=int(entrada.texto)
     area=lado*lado*lado
-    asistente.decir(u"Area="+str(area))
+    asistente.decir(u"Área="+str(area))
     asistente.sonreir()
     
 boton=pilas.interfaz.Boton("calcular")
-boton.y=10
+boton.y=-8
 boton.escala=1.5
 boton.conectar(calcular_area)
     
